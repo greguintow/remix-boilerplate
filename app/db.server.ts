a60580fe1617336395989e8aee0400dcadb92df4
Greg Teixeira <greg.teixeira123@gmail.com>
2022-08-28 19:03:39 -0300

    chore: fix eslint

--- app/db.server.ts
+++ app/db.server.ts
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 function getClient() {
+  // eslint-disable-next-line no-console
   console.log(`🔌 setting up prisma client`)
   const client = new PrismaClient()
 
